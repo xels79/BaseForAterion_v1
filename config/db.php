640f21c1653ae34fa6e1b7ba5cd524e0ee9c6a65
xels79 <xel_s@mail.ru>
2017-07-15 19:18:47 +0300

    Первая загрузка

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=xels79_baseaster',
+    'username' => 'xels79_baseaster',
+    'password' => 'baseAsterion',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'aplicationName'=>'База данных',
+    'id'=>'AsterionDB',
+    'isBlocked'=>false,
+    'extHost'=>'http://93.185.189.210:28080/',
+    'sqlBackFolder'=>'baseSQL',
+    'saveBaseTimeOut'=>604800,
+    'sqlBackFileName'=>'asterionBase.sql',
+    'localH'=>true
+];
